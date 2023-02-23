1a57675475f041c8b95a5ac3e3152d5665615fe7
kevinleo1996f <kevinleo1996f@gmail.com>
2023-02-23 13:12:38 +1100

    react file

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "ReactDOM";
-
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
