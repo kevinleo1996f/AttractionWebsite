ab455ae6f817b9fb7beef08e8711a47a9c4da390
kevinleo1996f <kevinleo1996f@gmail.com>
2023-02-23 15:48:30 +1100

    registration1

--- project/src/components/registration.jsx
+++ project/src/components/registration.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
-function Registration(){
-    
+
+function listener(){
+  
+    const a = document.getElementById('regis')
+    a.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const nameInput = a.elements['name'];
+        const user = nameInput.value;
+        console.log(user);
+    })
+}
+
+export default function Reg(){
+
+    return(
+        <form id="regis">
+            <input type="text" id="name" name="username" placeholder="Username"/>
+            <button onClick={listener} value="Submit" type="submit">Submit</button>
+        </form>
+    )
 }
 
